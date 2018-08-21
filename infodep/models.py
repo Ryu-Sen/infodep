10a90e960dfbda420fa7dd87cfde57e4ce75c9be
Ryunosuke Senda <42127342+List-Senda@users.noreply.github.com>
2018-08-21 11:12:35 +0900

    Initial commit

---
+++ infodep/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Kitting(models.Model):
+    job_type = models.CharField(max_length=50, null=True)
+    unit_number = models.CharField(max_length=50, null=True)
+    account_name = models.CharField(max_length=30, null=True)
+    user_name = models.CharField(max_length=50, null=True)
+    furigana = models.CharField(max_length=50, null=True)
+    note = models.CharField(max_length=300, null=True)
+    status = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.account_name
+
+    class Meta:
+        db_table = 'infodep_kitting'
+
---
+++ infodep/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponseRedirect
+from .models import Kitting
+import datetime
+
+from .forms import RenewBookForm
+
+
+def kitting(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    num_entry = Kitting.objects.all().count()
+
+    extra_context = {}
+    kitting = Kitting.objects.filter(status="kit")
+    extra_context['kitting'] = kitting
+
+    # Render the HTML template index.html with the data in the context variable
+    return render_to_response(
+        'kitting.html',
+        extra_context
+    )
+
+
+def returns(request):
+    extra_context = {}
+
+    kitting = Kitting.objects.filter(status="returned")
+    extra_context['kitting'] = kitting
+
+    return render_to_response(
+        'returns.html',
+        extra_context
+    )
